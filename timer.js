480838788917994f8dd1e986989ca3b382e4037a
Mubashir Chandle <61664272+mubashirchandle@users.noreply.github.com>
2021-03-18 18:52:45 +0530

    Restart timer on losing focus

--- timer.js
+++ timer.js
@@ -1,11 +1,21 @@
-var timerDuration = 60;
-var timeLeft = timerDuration;
+var TIMER_DURATION = 60;
 
-var timer = setInterval(() => {
-    if (timeLeft <= 0) {
-        clearInterval(timer);
-    }
+function startTimer() {
+    var timeLeft = TIMER_DURATION;
 
-    document.getElementById("timer_text").innerHTML = timeLeft;
-    timeLeft -= 1;
-}, 1000);
+    var timer = setInterval(() => {
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+        }
+
+        document.getElementById("timer_text").innerHTML = timeLeft;
+        timeLeft -= 1;
+
+        if (!document.hasFocus()) {
+            startTimer();
+            timeLeft = 0;
+        }
+    }, 1000);
+}
+
+startTimer();
